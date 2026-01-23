f7dd40e773e4461c8bbaabd2d9970029fbc480b6
Yuzu-Lemon0822 <yuzulemon140416@gmail.com>
2026-01-23 12:25:08 +0900

    update

--- main.js
+++ main.js
@@ -8,16 +8,26 @@ const btn = document.getElementById("randomBtn");
 const randomPick = (arr) =>
   arr[Math.floor(Math.random() * arr.length)];
 
-btn.addEventListener("click", () => {
-  const result = `
-性別：${randomPick(data.性別)}
-髪型：${randomPick(data.髪型)}
-表情：${randomPick(data.表情)}
-服装：${randomPick(data.服装)}
-アクセサリ：${randomPick(data.アクセサリ)}
-雰囲気：${randomPick(data.雰囲気)}
-シチュエーション：${randomPick(data.シチュエーション)}
-`.trim();
+function pick() {
+  let answer = `
+    性別：${randomPick(data.性別)}
+    髪型：${randomPick(data.髪型)}
+    表情：${randomPick(data.表情)}
+    アクセサリ：${randomPick(data.アクセサリ)}
+  `
+  answer += `服装(上)：${randomPick(data["服装(上)"])}`
+  if (Math.random < 0.5) {
+    answer += `服装(上)(重ね着)：${randomPick(data["服装(上)(重ね着)"])}`
+  }
+  answer += `服装(下)：${randomPick(data["服装(下)"])}`
+  if (Math.random < 0.5) {
+    answer += `服装(下)(重ね着)：${randomPick(data["服装(下)(重ね着)"])}`
+  }
 
+  return answer;
+}
+
+btn.addEventListener("click", () => {
+  const result = pick().trim();
   output.textContent = result;
 });
